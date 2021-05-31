9093b1f7692aefc7afa32993aee0071e727ca097
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-05-31 20:05:45 +0300

    Server logic is separated in another project and class

---
+++ CSharpWebServer/CSharpWebServer.Server/HttpServer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWebServer.Server
+{
+    public class HttpServer
+    {
+        private IPAddress ipAddress;
+        private int port;
+        private readonly TcpListener listener;
+
+        public HttpServer(string ipAddress,int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.listener = new TcpListener(this.ipAddress,this.port);
+        }
+
+        public async Task Start()
+        {
+
+            listener.Start();
+
+            Console.WriteLine($"Server started on port {port}...");
+            Console.WriteLine("Awaiting for requests...");
+
+            while (true)
+            {
+                var connection = await listener.AcceptTcpClientAsync();
+
+                var networkStream = connection.GetStream();
+
+                var request = await this.ReadRequest(networkStream);
+                Console.WriteLine(request);
+
+                await this.WriteResponse(networkStream);
+
+                connection.Close();
+            }
+        }
+
+        private async Task<string> ReadRequest(NetworkStream networkStream)
+        {
+            var bufferLenght = 1024;
+            var buffer = new byte[bufferLenght];
+            var totalBytesRead = 0;
+            var requestBuilder = new StringBuilder();
+            while (networkStream.DataAvailable)
+            {
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLenght);
+
+                totalBytesRead += bytesRead;
+
+                if (totalBytesRead > 10 * 1024)
+                {
+                    //TODO: Implement HTTP status code 431
+                }
+                requestBuilder.AppendLine(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+            return requestBuilder.ToString();
+        }
+        private async Task WriteResponse(NetworkStream networkStream)
+        {
+            var content = $@"<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel=""icon"" href=""data:,"">
+</head>
+   
+
+<body>
+<h1>Heading of my server</h1>
+</body>
+   
+
+</html>";
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+
+            var response = @$"HTTP/1.1 200 OK
+Server: MyServer
+Date: {DateTime.UtcNow:r}
+Content-Length: {contentLength}
+Content-Type: text/html;charset=utf-8
+
+{content}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            await networkStream.WriteAsync(responseBytes);
+        }
+    }
+}
--- CSharpWebServer/CSharpWebServer/StartUp.cs
+++ CSharpWebServer/CSharpWebServer/StartUp.cs
@@ -1,81 +1,16 @@
 ﻿namespace WebServer
 {
-    using System;
-    using System.Net;
-    using System.Net.Sockets;
-    using System.Text;
+    using CSharpWebServer.Server;
     using System.Threading.Tasks;
     public class StartUp
     {
         public static async Task Main()
         {
             // http://localhost:1234/
-
-            var address = IPAddress.Parse("127.0.0.1");
+            var ip = "127.0.0.1";
             var port = 4000;
-
-            var serverListener = new TcpListener(address, port);
-
-            serverListener.Start();
-
-            Console.WriteLine($"Server started on port {port}...");
-            Console.WriteLine("Awaiting for requests...");
-
-            while (true)
-            {
-                var connection = await serverListener.AcceptTcpClientAsync();
-
-                var networkStream = connection.GetStream();
-
-                var bufferLenght = 1024;
-                var buffer = new byte[bufferLenght];
-                var totalBytesRead = 0;
-                var requestBuilder = new StringBuilder();
-                while (networkStream.DataAvailable)
-                {
-                    var bytesRead = await networkStream.ReadAsync(buffer,0,bufferLenght);
-
-                    totalBytesRead += bytesRead;
-
-                    if (totalBytesRead > 10 * 1024)
-                    {
-                        //TODO: Implement HTTP status code 431
-                        connection.Close();
-                    }
-                    requestBuilder.AppendLine(Encoding.UTF8.GetString(buffer,0,bytesRead));
-                }
-                Console.WriteLine(requestBuilder.ToString());
-
-                var content = $@"<!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-    <link rel=""icon"" href=""data:,"">
-</head>
-   
-
-<body>
-<h1>Heading of my server</h1>
-</body>
-   
-
-</html>";
-                var contentLength = Encoding.UTF8.GetByteCount(content);
-
-                var response = @$"HTTP/1.1 200 OK
-Server: MyServer
-Date: {DateTime.UtcNow:r}
-Content-Length: {contentLength}
-Content-Type: text/html;charset=utf-8
-
-{content}";
-
-                var responseBytes = Encoding.UTF8.GetBytes(response);
-
-                await networkStream.WriteAsync(responseBytes);
-
-                connection.Close();
-            }
+            var server = new HttpServer(ip,port);
+            await server.Start();
         }
     }
 }
