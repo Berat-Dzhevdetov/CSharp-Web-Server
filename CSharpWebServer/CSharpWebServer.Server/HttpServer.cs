72c39f8eadcc429927947b0fe2e4fcc805d4a6db
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-06-04 09:40:35 +0300

    Added header just to see how to add styling but  for now it's only hardcoded

--- CSharpWebServer/CSharpWebServer.Server/HttpServer.cs
+++ CSharpWebServer/CSharpWebServer.Server/HttpServer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace CSharpWebServer.Server
 {
     using CSharpWebServer.Server.Http;
@@ -85,13 +85,73 @@ namespace CSharpWebServer.Server
     <title>Home</title>
     <link rel=""icon"" href=""data:,"">
 </head>
-   
+<style>
+* {{ box-sizing: border-box; }}
+
+body {{
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}}
+
+.header {{
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+}}
+
+.header a {{
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}}
+
+.header a.logo {{
+  font-size: 25px;
+  font-weight: bold;
+}}
+
+.header a:hover {{
+  background-color: #ddd;
+  color: black;
+}}
+
+.header a.active {{
+  background-color: dodgerblue;
+  color: white;
+}}
+
+.header-right {{
+  float: right;
+}}
+
+@media screen and (max-width: 500px) {{
+  .header a {{
+    float: none;
+    display: block;
+    text-align: left;
+  }}
+  
+  .header-right {{
+    float: none;
+  }}
+}}
+</style>
 
 <body>
-<h1>Heading of my server</h1>
+<div class=""header"">
+  <a href=""#default"" class=""logo"">CompanyLogo</a>
+  <div class=""header-right"">
+    <a class=""active"" href=""#home"">Home</a>
+    <a href=""#contact"">Contact</a>
+    <a href=""#about"">About</a>
+  </div>
+</div>
 </body>
-   
-
 </html>";
             var contentLength = Encoding.UTF8.GetByteCount(content);
 
