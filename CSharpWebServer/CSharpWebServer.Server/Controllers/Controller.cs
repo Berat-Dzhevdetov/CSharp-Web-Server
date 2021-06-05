8b6b8b85ced8471dc0316ad6ddc765293b4709cb
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-06-05 10:30:21 +0300

    I made a return view method in base Controller
    that looks for a folder, that should be named as the controller itself, then gets the file with the same name as the method

--- CSharpWebServer/CSharpWebServer.Server/Controllers/Controller.cs
+++ CSharpWebServer/CSharpWebServer.Server/Controllers/Controller.cs
@@ -1,5 +1,10 @@
 ﻿namespace CSharpWebServer.Server.Controllers
 {
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
     using CSharpWebServer.Server.Http;
     using CSharpWebServer.Server.Responses;
 
@@ -18,5 +23,38 @@
 
         protected HttpResponse Redirect(string location)
             => new RedirectResponse(location);
+
+        protected HttpResponse View()
+        {
+            StackTrace trace = new StackTrace();
+            StackFrame frame = trace.GetFrame(1); // 0 will be the inner-most method
+            MethodBase method = frame.GetMethod();
+            var controllerName = GetNameOfController(method.DeclaringType.Name).Trim();
+            if (controllerName == null) new NotFoundResponse();
+
+            var methodName = trace.GetFrame(1).GetMethod().Name.Trim();
+
+            string codeBase = Assembly.GetExecutingAssembly().Location;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var dirName =  Path.GetDirectoryName(path);
+
+            var pathToFile = Path.GetFullPath(Path.Combine(dirName,@"..\..\..\","Views",controllerName));
+
+            if (!Directory.Exists(pathToFile)) return new NotFoundResponse();
+
+            var filePath = Directory.GetFiles(pathToFile, $"{methodName.ToLower()}.cshtml")[0];
+            var html = File.ReadAllText(filePath);
+            return new HtmlResponse(html);
+        }
+        private string GetNameOfController(string fullname)
+        {
+            if (fullname.Length <= 10)
+            {
+                return null;
+            }
+            var indexOfControllerWord = fullname.IndexOf("Controller");
+            return fullname.Substring(0, indexOfControllerWord);
+        }
     }
 }
